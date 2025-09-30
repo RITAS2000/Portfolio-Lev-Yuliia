50278069cc68618ff230cf872be6c019b9066c34
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-09-30 12:23:59 +0300

    fix: bg img header

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   const footer = document.createElement('footer');
   footer.classList.add('footer-section');
   footer.innerHTML = `   <a href="../../../index.html" class="logo-link"><div class="logo-container">
-   <img src="logo.jpg" alt="Yuliia Lev" class="logo">
+   <img src="/logo.jpg" alt="Yuliia Lev" class="logo">
     <div class="logo-text-container">
     <p class="logo-name">Lev Yuliia</p>
     <p class="logo-text">Full-Stack</p></div>
--- src/js/components/header.js
+++ src/js/components/header.js
@@ -2,7 +2,7 @@ export default function Header() {
   const header = document.createElement('header');
   header.innerHTML = `<section class="section-big">
     <a href="../../../index.html" class="logo-link"><div class="logo-container">
-   <img src="logo.jpg" alt="Yuliia Lev" class="logo">
+   <img src="/logo.jpg" alt="Yuliia Lev" class="logo">
     <div class="logo-text-container">
     <p class="logo-name">Lev Yuliia</p>
     <p class="logo-text">Full-Stack</p></div>
@@ -10,7 +10,7 @@ export default function Header() {
     </a>
     <button type="button" class="burger">
     <svg class="icon" width="32" height="32">
-  <use href="icons.svg#icon-burger"></use>
+  <use href="/icons.svg#icon-burger"></use>
   </svg>
   </button>
   </section>`;
