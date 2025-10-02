0620c4ec192c5a9ff1d928664ae88a35b2caa2c9
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-10-02 10:42:11 +0300

    validate form

--- src/js/components/form.js
+++ src/js/components/form.js
@@ -4,20 +4,56 @@ export default function Form() {
 
   formSection.addEventListener('input', event => {
     const input = event.target;
+
     if (input.classList.contains('input')) {
       input.value = input.value.replace(/[<>\/\\{}[\];$]/g, '');
 
+      const span = input.nextElementSibling;
+
+      if (!span) return;
+
       if (input.value.length === 0) {
         input.classList.remove('touched-invalid', 'touched-valid');
-      } else if (
-        input.value.length < (input.minLength || 0) ||
-        !input.checkValidity()
-      ) {
-        input.classList.add('touched-invalid');
-        input.classList.remove('touched-valid');
-      } else {
-        input.classList.remove('touched-invalid');
-        input.classList.add('touched-valid');
+        span.textContent = '';
+        return;
+      }
+
+      if (input.type === 'text') {
+        if (input.value.length < (input.minLength || 0)) {
+          input.classList.add('touched-invalid');
+          input.classList.remove('touched-valid');
+          span.textContent = `Enter from ${input.minLength} to ${input.maxLength} characters`;
+          span.style.color = 'red';
+        } else {
+          input.classList.remove('touched-invalid');
+          input.classList.add('touched-valid');
+          span.textContent = 'Looks good!';
+          span.style.color = 'green';
+        }
+      } else if (input.type === 'email') {
+        if (!input.checkValidity()) {
+          input.classList.add('touched-invalid');
+          input.classList.remove('touched-valid');
+          span.textContent = 'Please enter a valid email (e.g. name@gmail.com)';
+          span.style.color = '#f51111';
+        } else {
+          input.classList.remove('touched-invalid');
+          input.classList.add('touched-valid');
+          span.textContent = 'Email looks valid';
+          span.style.color = '#2de92d';
+        }
+      } else if (input.name === 'message') {
+        if (!input.checkValidity()) {
+          input.classList.add('touched-invalid');
+          input.classList.remove('touched-valid');
+          span.textContent = `Enter from ${input.minLength} to ${input.maxLength} characters`;
+          span.style.color = '#f51111';
+        } else {
+          input.classList.remove('touched-invalid');
+          input.classList.add('touched-valid');
+          span.textContent = 'Looks good!';
+          span.style.color = '#2de92d';
+        }
       }
     }
   });
@@ -31,14 +67,17 @@ export default function Form() {
     </svg>
     <input type="text" name="text" class="input" minlength="2" 
      maxlength="36" required />
+     <span class="js-input-message"></span>
     </label>
     <label>Email
    <svg class="form-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#0a3b0a"><g fill="none" stroke="#0a3b0a" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="m7 9l5 3.5L17 9"/><path d="M2 17V7a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2Z"/></g></svg>
     <input type="email" name="email" class="input" pattern="^[a-zA-Z0-9._\\-]+@[a-zA-Z0-9._\\-]+\\.[a-zA-Z]{2,4}$" maxlength="64" required>
+    <span class="js-input-message"></span>
     </label>
     <label>Message
     <svg class="form-icon" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 576 512"><path d="m402.3 344.9l32-32c5-5 13.7-1.5 13.7 5.7V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h273.5c7.1 0 10.7 8.6 5.7 13.7l-32 32c-1.5 1.5-3.5 2.3-5.7 2.3H48v352h352V350.5c0-2.1.8-4.1 2.3-5.6zm156.6-201.8L296.3 405.7l-90.4 10c-26.2 2.9-48.5-19.2-45.6-45.6l10-90.4L432.9 17.1c22.9-22.9 59.9-22.9 82.7 0l43.2 43.2c22.9 22.9 22.9 60 .1 82.8zM460.1 174L402 115.9L216.2 301.8l-7.3 65.3l65.3-7.3L460.1 174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8 0L436 82l58.1 58.1l30.9-30.9c4-4.2 4-10.8-.1-14.9z"/></svg>
-    <textarea name="message" rows="8" class="textarea" maxlength="560" required></textarea></label>
+    <textarea name="message" rows="8" class="textarea input" minlength="5"maxlength="560" required></textarea>
+    <span class="js-textaera-message"></span></label>
     <button type="submit" class="button"><span>Send</span></button>
     </form>`;
 
