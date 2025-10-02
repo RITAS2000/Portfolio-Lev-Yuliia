8864b472f83b53f65d0bc6301d0bc6cb2c5d7ccc
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-10-02 12:44:34 +0300

    test resumeimg

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,5 +1,6 @@
 export default function Header() {
   const header = document.createElement('header');
+
   header.innerHTML = `<section class="section-big">
     <a href="../../../index.html" class="logo-link"><div class="logo-container">
    <img src="/logo.jpg" alt="Yuliia Lev" class="logo">
@@ -13,6 +14,53 @@ export default function Header() {
   <use href="/icons.svg#icon-burger"></use>
   </svg>
   </button>
+ <div class="backdrop-burger" id="mobile-menu">
+  <div class="modal">
+    <button class="modal-close-btn" type="button" aria-label="Modal close button">
+      <svg class="icon-close" width="28" height="28">
+        <use href="/img/sprite.svg#icon-close"></use>
+      </svg>
+    </button>
+    <nav><ul>
+    <li>About My</li>
+    <li>Portfolio</li>
+    <li>Connect</li>
+    </ul></nav>
+  </div>
+</div>
   </section>`;
+
+  const modalWindow = header.querySelector('.backdrop-burger');
+  const openMenuBtn = header.querySelector('.burger');
+  const closeModalBtn = header.querySelector('.modal-close-btn');
+
+  let scrollPosition = 0;
+
+  function interactionModalWindow() {
+    const isOpen = !modalWindow.classList.contains('is-open');
+
+    modalWindow.classList.toggle('is-open', isOpen);
+    openMenuBtn.setAttribute('aria-expanded', isOpen);
+
+    if (isOpen) {
+      scrollPosition = window.pageYOffset;
+      document.body.style.position = 'fixed';
+      document.body.style.top = `-${scrollPosition}px`;
+      document.body.style.left = '0';
+      document.body.style.right = '0';
+      document.body.style.width = '100%';
+    } else {
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.left = '';
+      document.body.style.right = '';
+      document.body.style.width = '';
+      window.scrollTo(0, scrollPosition);
+    }
+  }
+
+  openMenuBtn.addEventListener('click', interactionModalWindow);
+  closeModalBtn.addEventListener('click', interactionModalWindow);
+
   return header;
 }
