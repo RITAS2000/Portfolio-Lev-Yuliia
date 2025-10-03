55600bc922326ab26c01547eeb9a40c3ca0e36ad
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-10-03 15:21:06 +0300

    add hero 768

--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -1,13 +1,15 @@
 export default function Hero() {
   const heroSection = document.createElement('section');
   heroSection.classList.add('hero-section');
-  heroSection.innerHTML = `<div>
-  <h1 class="hero-titel">Hello, I'm Yuliia Lev — Full-Stack Developer.</h1>
+  heroSection.innerHTML = `<div class="hero-container">
+  <div class="hero-text-container"><h1 class="hero-titel">Hello, I'm Yuliia Lev — Full-Stack Developer.</h1>
   <p class="hero-text">Take a journey into my portfolio and discover what I build.</p>
-  <img src="photo_2025.jpg" class="hero-img" alt="Lev Yuliia" width="240" height="240">
+  </div>
+  <div class="hero-img-container"><img src="photo_2025.jpg"  alt="Lev Yuliia" width="201" height="201" class="hero-img"></div>
+  </div>
   <div class="hero-links">
   <a href="#resume" class="hero-link"><span>Resume</span></a>
   <a href="https://github.com/RITAS2000/Portfolio-Lev-Yuliia" class="gh-link" target="_blank" rel="noreferrer noopener"><img src="/gh-icon.jpg" width="40" height="40"></a>
-  </div></div>`;
+  </div>`;
   return heroSection;
 }
